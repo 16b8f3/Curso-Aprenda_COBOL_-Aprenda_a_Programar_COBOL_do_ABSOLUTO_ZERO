3a6b6c7ab4bbdad69fbe518e8f8aa08a226b3ef4
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-10-21 17:20:54 -0300

    O comando ON SIZE ERROR
    Tudo que tem calculo é bom usar o ON SIZE ERROR para evitar 'surpresas'

---
+++ Modulo_2/PROGONSIZEERROR.cbl
@@ -0,0 +1,43 @@
+      ******************************************************************
+      * Author:
+      * Date: 20/OUT/2022
+      * Purpose: Mostrar comandos COBOL
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGONSIZEERROR.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 WS-NUM-1                    PIC 9(02) VALUE ZEROS.
+       77 WS-NUM-2                    PIC 9(02) VALUE ZEROS.
+       77 WS-RESULT                   PIC 9(02) VALUE ZEROS.
+       PROCEDURE DIVISION.
+       P001-INICIO.
+
+            PERFORM P100-CALC.
+            PERFORM P999-FIM.
+
+       P100-CALC.
+
+            DISPLAY 'DIGITE O PRIMEIRO NUMERO: '
+            ACCEPT WS-NUM-1
+
+            DISPLAY 'DIGITE O SEGUNDO NUMERO.: '
+            ACCEPT WS-NUM-2
+
+            COMPUTE WS-RESULT = WS-NUM-1 * WS-NUM-2
+                    ON SIZE ERROR PERFORM P800-ERRO
+            END-COMPUTE
+
+            DISPLAY 'CALCULO OK! RESULTADO...: ' WS-RESULT
+            .
+
+       P800-ERRO.
+
+            DISPLAY 'ERRO NO CALCULO.'
+            PERFORM P999-FIM
+            .
+       P999-FIM.
+            STOP RUN.
+       END PROGRAM PROGONSIZEERROR.
